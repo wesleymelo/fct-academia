d455fe5e241333de6a7bd6b586e319805455d96a
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@c26082be-cf80-551b-86bc-6604f8ebecf3>
2012-05-04 21:50:29 +0000

    criação do filtro

---
+++ trunk/fct/WEB-INF/src/br/ucb/fct/filters/Filtro.java
@@ -0,0 +1,42 @@
+package br.ucb.fct.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import br.ucb.fct.acesso.Acesso;
+import br.ucb.fct.enuns.EnumAcesso;
+
+public class Filtro implements Filter{
+
+	@Override
+	public void destroy() {		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		Acesso acesso = (Acesso) req.getSession().getAttribute(EnumAcesso.ACESSO.toString());
+			
+		if(acesso!=null){
+			chain.doFilter(req,resp);
+			return;
+		}
+		resp.sendRedirect(req.getContextPath()+"/index.do");
+			
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+	
+}
