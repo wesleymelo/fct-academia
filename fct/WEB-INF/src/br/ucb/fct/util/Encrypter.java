8cb7dcbdd9837dcbabf4e16b92a08143f7e15942
wesley.melo.pereira <wesley.melo.pereira@gmail.com>
2012-05-04 03:52:18 +0000

    Classe Encriptadora de senha de acesso ao fct

---
+++ fct/WEB-INF/src/br/ucb/fct/util/Encrypter.java
@@ -0,0 +1,18 @@
+package br.ucb.fct.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import sun.misc.BASE64Encoder;
+
+public class Encrypter {
+	public static String encripta (String senha) {  
+        try {  
+            MessageDigest digest = MessageDigest.getInstance("MD5");  
+            digest.update(senha.getBytes()); 
+            return new BASE64Encoder().encode(digest.digest());  
+        } catch (NoSuchAlgorithmException e) {  
+            e.printStackTrace();  
+            return senha;  
+        }  
+    }  
+}
