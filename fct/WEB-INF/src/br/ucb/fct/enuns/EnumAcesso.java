1fa85c2f1197ece5095e8d39a6d38c46c28c87d2
wesley.melo.pereira <wesley.melo.pereira@gmail.com>
2012-05-05 14:18:30 +0000

    Alteração enumAcesso

--- fct/WEB-INF/src/br/ucb/fct/enuns/EnumAcesso.java
+++ fct/WEB-INF/src/br/ucb/fct/enuns/EnumAcesso.java
@@ -1,5 +1,23 @@
 package br.ucb.fct.enuns;
 
 public enum EnumAcesso {
-	ACESSO, USUARIO, SENHA;
+	ACESSO("acesso"), LOGIN("login"), SENHA("senha");
+	
+	private String chave;
+	
+	private EnumAcesso(String chave){
+		setChave(chave);
+	}
+
+	public String getChave() {
+		return chave;
+	}
+
+	public void setChave(String chave) {
+		this.chave = chave;
+	}
+	
+	
+	
+	
 }
