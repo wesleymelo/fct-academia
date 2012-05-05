15461b0dfd657d4aef5fa7bfddc9427c8fc7ca42
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@c26082be-cf80-551b-86bc-6604f8ebecf3>
2012-05-05 03:02:59 +0000

    implementação da classe AcessoBO

--- trunk/fct/WEB-INF/src/br/ucb/fct/acesso/AcessoBO.java
+++ trunk/fct/WEB-INF/src/br/ucb/fct/acesso/AcessoBO.java
@@ -1,5 +1,20 @@
 package br.ucb.fct.acesso;
 
-public class AcessoBO {
+import br.ucb.fct.exceptions.DAOException;
 
+public class AcessoBO {
+	private static AcessoDAO dao =  new AcessoDAOConexao();
+	
+	public static Acesso findByUsuarioAndSenha(String usuario, String senha) throws DAOException{
+		return getDao().findByUsuarioAndSenha(usuario, senha);
+	}
+
+	public static boolean insert(Acesso acesso) throws DAOException{
+		return getDao().insert(acesso);
+	}
+
+	private static AcessoDAO getDao() {
+		return dao;
+	}
+	
 }
