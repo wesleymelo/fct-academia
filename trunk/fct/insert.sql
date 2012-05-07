0f816ea47d463ecfa40798ad967af80b0d624ba3
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@c26082be-cf80-551b-86bc-6604f8ebecf3>
2012-05-07 22:26:17 +0000

    Alteração

---
+++ trunk/fct/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT into academia.enderecos VALUES(null, '10A99BF', '', '01', '92AB77', 'RA', '721202B9', 0);
+
+INSERT into academia.pessoas VALUES(null, 0, '2012-05-07', 'admin', '00000000000', 'A', '2012-05-07',
+                          '2811592', 'SSP-DF', 1, 'admin@admin.com', 'admin', 'admin', true);
+                          
+INSERT into academia.secretarias VALUES(1, 0, '00:00', '00:00', '2012-05-07' );
+
+INSERT into academia.acessos VALUES(null, 1, MD5(1212));
+                          
