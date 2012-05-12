3f6f3b796951d67fe0bfa05607d7aab5dc7c3a12
campelo.ian@gmail.com <campelo.ian@gmail.com@c26082be-cf80-551b-86bc-6604f8ebecf3>
2012-05-12 17:19:31 +0000

    Finalizando DAO de despesas!

--- trunk/fct/WEB-INF/src/br/ucb/fct/despesa/Despesa.java
+++ trunk/fct/WEB-INF/src/br/ucb/fct/despesa/Despesa.java
@@ -1,5 +1,71 @@
 package br.ucb.fct.despesa;
 
 public class Despesa {
-
+	private Integer idDespesa,quantidade;
+	private String descricao;
+	
+	
+	public Despesa(Integer idDespesa, String descricao, Integer quantidade) {
+		setIdDespesa(idDespesa);
+		setQuantidade(quantidade);
+		setDescricao(descricao);
+	}
+	public Integer getIdDespesa() {
+		return idDespesa;
+	}
+	public void setIdDespesa(Integer idDespesa) {
+		this.idDespesa = idDespesa;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result
+				+ ((idDespesa == null) ? 0 : idDespesa.hashCode());
+		result = prime * result
+				+ ((quantidade == null) ? 0 : quantidade.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Despesa other = (Despesa) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (idDespesa == null) {
+			if (other.idDespesa != null)
+				return false;
+		} else if (!idDespesa.equals(other.idDespesa))
+			return false;
+		if (quantidade == null) {
+			if (other.quantidade != null)
+				return false;
+		} else if (!quantidade.equals(other.quantidade))
+			return false;
+		return true;
+	}
+	
+	
 }
--- trunk/fct/WEB-INF/src/br/ucb/fct/despesa/DespesaDAOConexao.java
+++ trunk/fct/WEB-INF/src/br/ucb/fct/despesa/DespesaDAOConexao.java
@@ -1,5 +1,127 @@
 package br.ucb.fct.despesa;
 
-public class DespesaDAOConexao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import br.ucb.fct.connection.MyConnection;
+import br.ucb.fct.exceptions.DAOException;
+
+public class DespesaDAOConexao implements DespesaDAO{
+
+	@Override
+	public boolean insert(Despesa despesa) throws DAOException {
+		String sql="INSERT INTO DESPESAS(idDespesa, descricao, quantidade) VALUES(null,?,?);";
+		Connection con = null;
+		PreparedStatement ps = null;
+		int retorno;
+		try {
+			con=MyConnection.init();
+			ps=con.prepareStatement(sql);
+			ps.setString(2,despesa.getDescricao());
+			ps.setInt(3,despesa.getQuantidade());
+			retorno=ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! INSERT na TABELA DESPESAS. DATA("+new Date()+")");
+		}
+		finally{
+			MyConnection.closeConnection(con, ps);
+		}
+		return retorno==0?false:true;
+		
+		
+	}
+	public boolean delete(int id) throws DAOException {
+		String sql="DELETE FROM despesas WHERE idDespesa = ?;";
+		Connection con=null;
+		PreparedStatement ps=null;
+		int retorno;
+		try {
+			con=MyConnection.init();
+			ps=con.prepareStatement(sql);
+			ps.setObject(1,id);
+			retorno=ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! DELETE na TABELA despesas. DATA("+new Date()+")");
+		}finally{
+			MyConnection.closeConnection(con,ps);
+		}
+		return retorno ==0?false:true;//AJUSTAR AO FACTORY???
+	}
+
+	@Override
+	public boolean update(Despesa despesas, int id) throws DAOException {
+		String sql="UPDATE despesas SET quantidade=?,descricao=?;";
+		Connection con=null;
+		PreparedStatement ps=null;
+		int retorno=0;
+		try {
+			con=MyConnection.init();
+			ps=con.prepareStatement(sql);
+			ps.setInt(1,despesas.getQuantidade());
+			ps.setString(2,despesas.getDescricao());
+			retorno=ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! UPDATE na TABELA despesas. DATA("+new Date()+")");
+		}finally{
+			MyConnection.closeConnection(con, ps);
+		}
+		return retorno == 0?false:true;
+	}
+
+	@Override
+	public List<Despesa> selectAll() throws DAOException {
+		String sql="SELECT * FROM gastos;";
+		List<Despesa> despesas= new ArrayList<Despesa>();
+		Connection con=null;
+		Statement  stm=null;
+		ResultSet rs=null;
+		try {
+			con=MyConnection.init();
+			stm=con.createStatement();
+			rs=stm.executeQuery(sql);
+			while(rs.next()){
+				despesas.add(getDespesa(rs));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! SELECTALL na TABELA Despesas. DATA("+new Date()+")");
+		}finally{
+			MyConnection.closeConnection(con, stm, rs);
+		}
+		return despesas;
+	}
+	@Override
+	public Despesa selectById(int id) throws DAOException {
+		String sql="SELECT * FROM despesas WHERE idDespesa="+id+";";
+		Connection con=null;
+		Statement  stm=null;
+		ResultSet rs=null;
+		Despesa newDespesa=null;
+		try {
+			con=MyConnection.init();
+			stm=con.createStatement();
+			rs=stm.executeQuery(sql);
+			newDespesa=getDespesa(rs);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! SELECT BY ID NA TABELA DESPESAS. DATA("+new Date()+")");
+		}finally{
+			MyConnection.closeConnection(con, stm, rs);
+		}
+		return newDespesa;
+	}
+	
+	private Despesa getDespesa(ResultSet rs) throws SQLException {
+		return new Despesa(rs.getInt("idDespesa"),rs.getString("descricao"),rs.getInt("quantidade"));
+	}
+	
 
 }
