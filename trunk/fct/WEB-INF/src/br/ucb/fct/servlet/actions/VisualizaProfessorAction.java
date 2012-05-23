f2d61cfa41cd73e256a2625b6333ba93be9b60a8
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@c26082be-cf80-551b-86bc-6604f8ebecf3>
2012-05-23 04:06:20 +0000

    View de prof, secre, alun

--- trunk/fct/WEB-INF/src/br/ucb/fct/servlet/actions/VisualizaProfessorAction.java
+++ trunk/fct/WEB-INF/src/br/ucb/fct/servlet/actions/VisualizaProfessorAction.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import br.ucb.fct.util.Factory;
 
-public class VisualizaAlunoAction implements Action {
+public class VisualizaProfessorAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
 		
-		req.setAttribute("aluno", Factory.initAlunoDAO().selectById(Integer.parseInt(req.getParameter("id"))));
-		return "/view/admin/aluno/listaAlunos.do?show=true";
+		req.setAttribute("professor", Factory.initProfessorDAO().selectById(Integer.parseInt(req.getParameter("id"))));
+		return "/view/admin/professor/listaProfessores.do?show=true";
 
 	}
 
