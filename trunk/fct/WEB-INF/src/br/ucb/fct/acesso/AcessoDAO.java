ff76d6d144eeff9715aa696518d6e7ccfcaefb0d
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@c26082be-cf80-551b-86bc-6604f8ebecf3>
2012-05-30 06:57:59 +0000

    criação do acesso

--- trunk/fct/WEB-INF/src/br/ucb/fct/acesso/AcessoDAO.java
+++ trunk/fct/WEB-INF/src/br/ucb/fct/acesso/AcessoDAO.java
@@ -1,5 +1,7 @@
 package br.ucb.fct.acesso;
 
+import java.util.List;
+
 import br.ucb.fct.exceptions.DAOException;
 
 public interface AcessoDAO {
@@ -7,4 +9,7 @@ public interface AcessoDAO {
 	boolean insert(Acesso acesso) throws DAOException;
 	boolean delete(int id) throws DAOException;
 	boolean update(Acesso acesso, int id) throws DAOException;
+	List<Acesso> selectAll() throws DAOException;
+	List<Acesso> selectByNome(String nome) throws DAOException;
+	Acesso findByUsuario(Integer idPessoa) throws DAOException;
 }
--- trunk/fct/WEB-INF/src/br/ucb/fct/acesso/AcessoDAOConexao.java
+++ trunk/fct/WEB-INF/src/br/ucb/fct/acesso/AcessoDAOConexao.java
@@ -4,8 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 import br.ucb.fct.connection.MyConnection;
+import br.ucb.fct.envelope.Envelope;
 import br.ucb.fct.exceptions.DAOException;
 import br.ucb.fct.util.Factory;
 
@@ -33,10 +38,32 @@ public class AcessoDAOConexao implements AcessoDAO {
 		}
 		return acesso;
 	}
+	
+	@Override
+	public Acesso findByUsuario(Integer idPessoa)
+			throws DAOException {
+		String sql = "SELECT * FROM acessos WHERE idPessoa = ?;";
+		Connection conn;
+		PreparedStatement ps;
+		ResultSet rs;
+		Acesso acesso = null;
+		try {
+			conn = MyConnection.init();
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, idPessoa);
+			rs = ps.executeQuery();
+			if(rs.first())
+				acesso = new Acesso(rs.getInt("idAcesso"), Factory.initPessoaDAO().selectById(rs.getInt("idPessoa")));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e, "ERRO! FINDBYUSUARIO na TABELA ACESSO. DATA("+new Date()+")");
+		}
+		return acesso;
+	}
 
 	@Override
 	public boolean insert(Acesso acesso) throws DAOException {
-		String sql = "INSERT INTO acesso(idAcesso, idPessoa, senha) VALUES(null, ?, ?)";
+		String sql = "INSERT INTO acessos (idAcesso, idPessoa, senha) VALUES(null, ?, MD5(?))";
 		Connection conn;
 		PreparedStatement ps;
 		int retorno;
@@ -44,7 +71,7 @@ public class AcessoDAOConexao implements AcessoDAO {
 		try {
 			conn = MyConnection.init();
 			ps = conn.prepareStatement(sql);
-			ps.setInt(1, acesso.getPessoa().getIdPessoa());
+			ps.setInt(1, acesso.getIdPessoa());
 			ps.setString(2, acesso.getSenha());
 			retorno = ps.executeUpdate();
 		} catch (SQLException e) {
@@ -64,6 +91,51 @@ public class AcessoDAOConexao implements AcessoDAO {
 	public boolean update(Acesso acesso, int id) throws DAOException {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	@Override
+	public List<Acesso> selectAll() throws DAOException {
+		String sql = "SELECT * FROM acessos;";
+		List<Acesso> acessos = new ArrayList<Acesso>();
+		Connection con = MyConnection.init();		
+		Statement stm = null;
+		ResultSet rs = null;
+		try {
+			stm = con.createStatement();
+			rs = stm.executeQuery(sql);
+			while(rs.next())
+				acessos.add(getAcesso(rs));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! SELECTALL na TABELA ACESSOS. DATA("+new Date()+")");
+		}
+		return acessos;
+	}
+	
+	
+	@Override
+	public List<Acesso> selectByNome(String nome) throws DAOException {
+		String sql = "SELECT * FROM acessos a, pessoas p WHERE a.idPessoa = p.idPessoa AND nome LIKE'%"+nome+"%';";
+		List<Acesso> acessos = new ArrayList<Acesso>();
+		Connection con = MyConnection.init();		
+		Statement stm = null;
+		ResultSet rs = null;
+		try {
+			stm = con.createStatement();
+			rs = stm.executeQuery(sql);
+			while(rs.next())
+				acessos.add(getAcesso(rs));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e,"ERRO! SELECTBYNOME na TABELA ACESSOS. DATA("+new Date()+")");
+		}
+		return acessos;
+		
+	}
+	
+	public static Acesso getAcesso(ResultSet rs) throws SQLException{
+		return new Acesso(rs.getInt("idAcesso"), 
+				            Factory.initPessoaDAO().selectById(rs.getInt("idPessoa")));
 	}
 	
 }
