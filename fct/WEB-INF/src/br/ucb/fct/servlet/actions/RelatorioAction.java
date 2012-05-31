f835e59ec4f5697bc72932556147b629fc34fc82
campelo.ian <campelo.ian@gmail.com>
2012-05-31 08:33:07 +0000

    relatorios

--- fct/WEB-INF/src/br/ucb/fct/servlet/actions/RelatorioAction.java
+++ fct/WEB-INF/src/br/ucb/fct/servlet/actions/RelatorioAction.java
@@ -22,51 +22,47 @@ public class RelatorioAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
-		//String var=req.getParameter("relatorio");
-		//System.out.println(EnumRelatorio.var.getRelatorio());
-		try {
-			//			JasperPrint print = JasperFillManager.fillReport(req.getSession().
-			//					getServletContext().getRealPath(EnumRelatorio.valueOf(req.getParameter("relatorio")).getRelatorio())/*chama relatorio*/
-			//					, null, 
-			//					new JRBeanCollectionDataSource(null));//puxando do banco...(fazer aqui o select)
 
+		try {
 
 			String url= new String();
-			List<?> acessa= null;
+			List<?> lista= null;
 			Integer i=Integer.parseInt(req.getParameter("relatorio"));
 			switch (i) {
 			case 1:
 				url=EnumRelatorio.ALUNO.getRelatorio();
-				acessa=Factory.initAlunoDAO().selectAll();
+				lista=Factory.initAlunoDAO().selectAll();
 				break;
 			case 2:
 				url=EnumRelatorio.PROFESSOR.getRelatorio();
-				acessa=Factory.initProfessorDAO().selectAll();
+				lista=Factory.initProfessorDAO().selectAll();
 				break;
 			case 3:
 				url=EnumRelatorio.TURMA.getRelatorio();
-				acessa=Factory.initTurmaDAO().selectAll();
+				lista=Factory.initTurmaDAO().selectAll();
 				break;
 			case 4:
 				url=EnumRelatorio.MODALIDADE.getRelatorio();
-				acessa=Factory.initModalidadeDAO().selectAll();
+				lista=Factory.initModalidadeDAO().selectAll();
 				break;
 			case 5:
 				url=EnumRelatorio.PAGAMENTO.getRelatorio();
-				acessa=Factory.initPagamentoDAO().selectAll();
+				lista=Factory.initPagamentoDAO().selectAll();
 				break;
 			case 6:
 				url=EnumRelatorio.GASTO.getRelatorio();
-				acessa=Factory.initGastoDAO().selectAll();
+				lista=Factory.initGastoDAO().selectAll();
 				break;
 			case 7:
 				url=EnumRelatorio.DESPESA.getRelatorio();
-				acessa=Factory.initDespesaDAO().selectAll();
+				lista=Factory.initDespesaDAO().selectAll();
 			}
+			System.out.println(req.getSession().getServletContext().getRealPath(url));
+			System.out.println("QUANTIDADE DE REG: "+lista.size());
 			JasperPrint print = JasperFillManager.fillReport(req.getSession().
 					getServletContext().getRealPath(url)/*chama relatorio*/
 					, null, 
-					new JRBeanCollectionDataSource(acessa));//puxando do banco...(fazer aqui o select)
+					new JRBeanCollectionDataSource(lista));//puxando do banco...(fazer aqui o select)
 			OutputStream outStream;
 			outStream = resp.getOutputStream();
 			resp.setContentType("application/pdf");
